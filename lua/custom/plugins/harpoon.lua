ccd7692e5b728f85827379ae030e55b02280365e
boris orlov <boris.o@changenow.io>
2024-10-16 23:44:03 +0300

    removed telescope from harpoon deps

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -11,32 +11,13 @@ return {
     -- REQUIRED
     harpoon:setup {}
     -- REQUIRED
-    --
-    local conf = require('telescope.config').values
-    local function toggle_telescope(harpoon_files)
-      local file_paths = {}
-      for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-      end
-
-      require('telescope.pickers')
-        .new({}, {
-          prompt_title = 'Harpoon',
-          finder = require('telescope.finders').new_table {
-            results = file_paths,
-          },
-          previewer = conf.file_previewer {},
-          sorter = conf.generic_sorter {},
-        })
-        :find()
-    end
 
     vim.keymap.set('n', '<leader>a', function()
       harpoon:list():add()
     end)
     vim.keymap.set('n', '<C-e>', function()
-      toggle_telescope(harpoon:list())
-    end, { desc = 'Open harpoon window' })
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
 
     vim.keymap.set('n', '<C-j>', function()
       harpoon:list():select(1)
@@ -47,7 +28,7 @@ return {
     vim.keymap.set('n', '<C-l>', function()
       harpoon:list():select(3)
     end)
-    vim.keymap.set('n', '<C-;>', function()
+    vim.keymap.set('n', '<C-i>', function()
       harpoon:list():select(4)
     end)
 
